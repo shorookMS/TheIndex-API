b277eac4010bf7dfeb9b9f7a1f17f9cbac244933
Octowl <alsaff1987@gmail.com>
2018-02-17 20:45:29 +0300

    Create Book and Author models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        ordering = ['-last_name', '-first_name']
+
+
+class Book(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-title', ]

6ae1f23cd7b1a14263de38a625a5258efc1d21aa
Octowl <alsaff1987@gmail.com>
2018-02-17 21:06:54 +0300

    Add serializers

---
+++ books/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = '__all__'

192df338efb2762332265eed1c6546b07284d1ec
Octowl <alsaff1987@gmail.com>
2018-03-02 21:05:26 +0300

    User new serializers in viewsets

--- books/views.py
+++ books/views.py
@@ -1,26 +1,26 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, serializers
 
 from .models import Author, Book
-from .serializers import AuthorSerializer, BookSerializer
+from .serializers import AuthorListSerializer, AuthorDetailSerializer, BookSerializer
 
 
-class BookViewSet(mixins.CreateModelMixin,
-                  mixins.ListModelMixin,
-                  mixins.RetrieveModelMixin,
-                  viewsets.GenericViewSet):
+class BookViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows books to be viewed or added
+    API endpoint that allows books to be viewed
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
-class AuthorViewSet(mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
-                    mixins.RetrieveModelMixin,
-                    viewsets.GenericViewSet):
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows authors to be viewed or added
+    API endpoint that allows authors to be viewed
     """
     queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return AuthorListSerializer
+        if self.action == 'retrieve':
+            return AuthorDetailSerializer
+        return serializers.Default
